b6b00e652e0d6483e04d73be0571b5aac7614a6e
vasanth1066 <vasanthanuvavi@gmail.com>
2024-01-26 17:11:16 +0530

    first commit

---
+++ src/Components/Productlist.js
@@ -0,0 +1,19 @@
+function Productlist(props) {
+  return (
+    <div>
+      <div>
+        <h2>Electronics Items</h2>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+      <div>
+        <h2>Food Items</h2>
+      </div>
+      <div>
+        <h2>SkinCare Items</h2>
+      </div>
+    </div>
+  );
+}
+export default Productlist;
